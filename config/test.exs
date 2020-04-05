a32e6a21998c6d6867eebdd241c59bdfd7efbed8
Josh Chernoff <jchernoff@morphic.pro>
2020-04-05 15:13:26 -0700

    add ci

--- config/test.exs
+++ config/test.exs
@@ -5,7 +5,7 @@ config :dissolver, ecto_repos: [Dissolver.Repo]
 config :dissolver, Dissolver.Repo,
   username: "postgres",
   password: "postgres",
-  database: "dissolver_dev",
+  database: "dissolver_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
 
